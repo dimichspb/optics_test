7f3519c885fb7d567608b7a45da1bc26d1be0168
Dmitry Tarantin <dimichspb@gmail.com>
2017-01-26 21:43:13 +0300

    Adding README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+**OpticsPlanet test task**
--- web/index.php
+++ web/index.php
@@ -1,1 +1,9 @@
 <?php
+
+require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+
+use test\App;
+
+$app = new App();
+
+$app->run();
